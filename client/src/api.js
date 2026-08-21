985ebab335332ec7d5046ca9c0a4e71e375e937f
NeerajSkillsUp <thisis.neerajbhardwaj@gmail.com>
2026-08-21 14:27:07 +0530

    Add backend live services

--- client/src/api.js
+++ client/src/api.js
@@ -1,21 +1,16 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000', // Points to API Gateway
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000',
 });
 
-// Interceptor: Automatically attach JWT token to EVERY request if logged in
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+    if (token) config.headers.Authorization = `Bearer ${token}`;
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default api;
